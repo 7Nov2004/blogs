245d9bae777ff1eafbaba65fadfe3b828fc091fd
7Nov2004 <7Nov2004@users.noreply.github.com>
2026-09-04 13:47:18 +0530

    Update site domain to techverseblogs.in

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://techverse.in',
+  site: 'https://techverseblogs.in',
   integrations: [
     tailwind(),
     sitemap(),
