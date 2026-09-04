cd1cbd6c870784bc28c1ef49174afda02e92ec77
7Nov2004 <7Nov2004@users.noreply.github.com>
2026-09-04 15:19:44 +0530

    Add MDX support + Amazon affiliate links (tag: aayush7455-21)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://techverseblogs.in',
   integrations: [
+    mdx(),
     tailwind(),
     sitemap(),
   ],
